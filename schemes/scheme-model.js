86a9a6059622d5e230e9b5f2d94fcef7c6d6eaff
Wesley White <wesleyrwhite86@gmail.com>
2019-08-15 19:14:41 -0700

    completed helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -18,3 +18,26 @@ function findById(id) {
     .where({ id })
     .first();
 }
+
+function findSteps(scheme_id) {
+  return db('schemes')
+    .join('steps', 'schemes.id', 'steps.scheme_id')
+    .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where(scheme_id);
+}
+
+function add(scheme) {
+  return db('schemes').insert(scheme);
+}
+
+function update(id, changes) {
+  return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('schemes')
+    .where({ id })
+    .del();
+}
