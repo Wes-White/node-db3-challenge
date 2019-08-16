67e17ed9f3a12acb6d55f24030570368638dc956
Wesley White <wesleyrwhite86@gmail.com>
2019-08-15 18:40:56 -0700

    Added find and findById

---
+++ schemes/scheme-model.js
@@ -0,0 +1,20 @@
+const db = require('../dbConfig');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
+}
